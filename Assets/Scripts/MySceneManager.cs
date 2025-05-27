5a224f47dae2ee648a584e2ceaebe08007793e64
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-05-27 19:36:14 +0300

    Optimise My Scene Manager a bit.

--- Assets/Scripts/MySceneManager.cs
+++ Assets/Scripts/MySceneManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// Singleton class to manage current scene
 public class MySceneManager : MonoBehaviour {
     private static MySceneManager _instance;
+    private static Scene _currentScene;
 
     public static MySceneManager Instance {
         get {
@@ -12,14 +15,15 @@ public class MySceneManager : MonoBehaviour {
             var obj = new GameObject("MySceneManager");
             _instance = obj.AddComponent<MySceneManager>();
             DontDestroyOnLoad(obj);
+            // Get active scene on instance create
+            _currentScene = SceneManager.GetActiveScene();
 
             return _instance;
         }
     }
-
+    
     public void ChangeAllColorsInScene() {
-        var currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-        foreach (var gameObj in currentScene.GetRootGameObjects()) {
+        foreach (var gameObj in _currentScene.GetRootGameObjects()) {
             if (gameObj.TryGetComponent<SpriteRenderer>(out var sprite)) {
                 sprite.color = Random.ColorHSV();
             } 
