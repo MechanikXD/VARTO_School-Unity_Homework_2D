b62ed8081d26c54b9e02d47c9af12cdc7be36c4e
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-06-01 10:21:16 +0300

    Move platform logic into separate file

--- Assets/Scripts/Core/MySceneManager.cs
+++ Assets/Scripts/Core/MySceneManager.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 namespace Core {
+    using Platform;
+
     /// Singleton class to manage current scene
     public class MySceneManager : MonoBehaviour {
         private static MySceneManager _instance;
         private static Scene _currentScene;
-        private static List<GameObject> _platformPool;  // Pool of platform to build level from. (Should be on scene)
+        private static List<Platform> _platformPool;  // Pool of platform to build level from. (Should be on scene)
         private static Transform _playerPosition;
         private static float _platformRepositionDistance;   // Distance from platform to player when can safety reposition
 
@@ -29,9 +30,14 @@ namespace Core {
                 _instance = obj.AddComponent<MySceneManager>();
                 DontDestroyOnLoad(obj);
                 
-                _platformPool = GameObject.FindGameObjectsWithTag("Ground").ToList();
                 _playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
                 _currentScene = SceneManager.GetActiveScene();
+
+                _platformPool = new List<Platform>();
+                var groundObjects = GameObject.FindGameObjectsWithTag("Ground");
+                foreach (var ground in groundObjects) {
+                    _platformPool.Add(new Platform(ground));
+                }
 
                 if (Camera.main != null) {
                     var mainCamera = Camera.main;
@@ -51,45 +57,32 @@ namespace Core {
 
         private void Update() {
             for (var i = 0; i < _platformPool.Count; i++) {
-                if (_platformPool[i].transform.position.y < _playerPosition.position.y) {
-                    if (_platformPool[i].TryGetComponent<BoxCollider2D>(out var platformCollider)) {
-                        if (!platformCollider.enabled) {
-                            platformCollider.enabled = true;
-                        }
-                    }
+                // Player above platform -> activate platform collider
+                if (_platformPool[i].PlatformY < _playerPosition.position.y) {
+                    _platformPool[i].SetColliderActive(true);
                 }
 
-                if (PlatformOutsidePLayerVisibility(i)) {
-                    MovePlatform(i, GetCoordinatesForNextPlatform());
+                if (_platformPool[i].IsOutsidePlayerVisibility(_playerPosition.position.y,
+                        _platformRepositionDistance)) {
+                    RepositionPlatformOnIndex(i);
                 }
             }
         }
 
         public void SetupScene(Vector2 startPlatformCoordinates) {
             if (_platformPool == null) return;
-            _platformPool[0].transform.position = startPlatformCoordinates;
+            _platformPool[0].Move(startPlatformCoordinates);
             AdvanceIndexInPool();
             
             for (var i = 1; i < _platformPool.Count; i++) {
-                MovePlatform(_nextPlatformToMoveIndex, GetCoordinatesForNextPlatform());
+                RepositionPlatformOnIndex(i);
             }
-        }
-        // TODO: Move into separate Platform class
-        private void MovePlatform(int platformIndex, Vector2 newPosition) {
-            _platformPool[platformIndex].transform.position = newPosition;
-            
-            if (_platformPool[platformIndex].TryGetComponent<BoxCollider2D>(out var platformCollider)) {
-                platformCollider.enabled = false;
-            }
-            
-            AdvanceIndexInPool();
         }
         
         private Vector2 GetCoordinatesForNextPlatform() {
             var newX = SceneCenter +
                        Random.Range(-_platformMaxHorizontalStep, _platformMaxHorizontalStep);
-            var newY = _platformPool[GetHighestPlatformIndex()].transform.position.y +
-                       _platformVerticalStep;
+            var newY = _platformPool[GetHighestPlatformIndex()].PlatformY + _platformVerticalStep;
             return new Vector2(newX, newY);
         }
 
@@ -101,13 +94,13 @@ namespace Core {
             return _nextPlatformToMoveIndex - 1 < 0 ?
                 _platformPool.Count -1 : _nextPlatformToMoveIndex - 1;
         }
-        // TODO: Move into separate Platform class
-        private bool PlatformOutsidePLayerVisibility(int platformIndex) {
-            var distanceToPlatform = _playerPosition.position.y -
-                                     _platformPool[platformIndex].transform.position.y;
-            return distanceToPlatform >= _platformRepositionDistance + 2f;
-        }
 
+        private void RepositionPlatformOnIndex(int index) {
+            _platformPool[index].Move(GetCoordinatesForNextPlatform());
+            _platformPool[index].SetColliderActive(false);
+            AdvanceIndexInPool();
+        }
+        
         public void ChangeAllColorsInScene() {
             foreach (var gameObj in _currentScene.GetRootGameObjects()) {
                 if (gameObj.TryGetComponent<SpriteRenderer>(out var sprite)) {
