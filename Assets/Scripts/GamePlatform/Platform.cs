7f2804d77c2b0ba7758c63ee24b0b69c8d3df77b
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-06-16 14:22:14 +0300

    Follow-Up commit

---
+++ Assets/Scripts/GamePlatform/Platform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace GamePlatform {
+    public class Platform {
+        private readonly GameObject _platformObject;
+        private const float MarginToReposition = 2f; // To ensure that platforms disappear and appear outside player visibility
+
+        public float PlatformY => _platformObject.transform.position.y;
+        
+        public Platform(GameObject platformReference) {
+            _platformObject = Object.Instantiate(platformReference);
+        }
+        
+        public bool IsOutsidePlayerVisibility(float playerY, float playerToBottomScreenDist) {
+            var distanceToPlatform = playerY - _platformObject.transform.position.y;
+            return distanceToPlatform >= playerToBottomScreenDist + MarginToReposition;
+        }
+        
+        public void Move(Vector2 newPosition) {
+            _platformObject.transform.position = newPosition;
+        }
+
+        public void SetColliderActive(bool isEnables) {
+            if (!_platformObject.TryGetComponent<BoxCollider2D>(out var collider)) {
+                return;
+            }
+
+            if (collider.enabled != isEnables) collider.enabled = isEnables;
+        }
+    }
+}
