2f0bdede4c7696a0144cd7e217b8691b972b2145
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-06-21 17:06:47 +0300

    Add game over screen view

---
+++ Assets/UI/View/GameOverScreenView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.View {
+    public class GameOverScreenView : MonoBehaviour {
+        [SerializeField] private Button restartButton;
+        [SerializeField] private Button exitButton;
+
+        [SerializeField] private TMP_Text currentScoreText;
+        [SerializeField] private TMP_Text bestScoreText;
+        [SerializeField] private TMP_Text currentHeightText;
+        [SerializeField] private TMP_Text bestHeightText;
+    }
+}
